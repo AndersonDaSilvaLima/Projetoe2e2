528b20347576ade0e4c5fcfd171fea3051415807
AndersonDaSilvaLima <anderson.ferlima9@gmail.com>
2022-01-18 21:11:53 -0300

    Teste de uma funcionalidade x

--- TesteTeste/src/test/java/Pages/hello.java
+++ TesteTeste/src/test/java/Pages/hello.java
@@ -4,10 +4,8 @@ public class hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello word");
-		System.out.println("Ola git");
-		System.out.println("Ola github");
-		System.out.println("Teste, teste");
-		System.out.println("Teste de syso");
+	    System.out.println(" Somente duas linhas mesmo");
+		
 		
 
 	}
