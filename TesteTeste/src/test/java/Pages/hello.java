25053cdcc90e854c1350e14a812358822aa23935
AndersonDaSilvaLima <anderson.ferlima9@gmail.com>
2022-01-23 00:01:03 -0300

    Testando novos comandos do commits

--- TesteTeste/src/test/java/Pages/hello.java
+++ TesteTeste/src/test/java/Pages/hello.java
@@ -4,7 +4,8 @@ public class hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello word");
-	    System.out.println(" Somente duas linhas mesmo");
+	    System.out.println("Somente duas linhas mesmo");
+	    System.out.println("Realizando novos testes dos commits");
 		
 		
 
