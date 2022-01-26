2ec6fae6fdbfcf84782be57431d902275b6b6325
AndersonDaSilvaLima <anderson.ferlima9@gmail.com>
2022-01-25 23:00:56 -0300

    private

--- TesteTeste/src/test/java/ignorar/ignorar.java
+++ TesteTeste/src/test/java/ignorar/ignorar.java
@@ -5,6 +5,7 @@ public class ignorar {
 	public static void main(String[] args) {
 		System.out.println("ignorar esse arquivos");
 		System.out.println("alteração no meu arquivo");
+		System.out.println("private branch");
 
 }
 
